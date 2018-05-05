634cbea8bf75452503c794361a6324fbc23eb65d
koleopteros <11413813+koleopteros@users.noreply.github.com>
2018-05-05 14:28:07 +0000

    Update camera_test.py
    Changed camera_test.py to hold testing functions

--- camera_test.py
+++ camera_test.py
@@ -2,39 +2,55 @@ from picamera import PiCamera
 from time import sleep
 import os
 
-'''
-This script tests the camera's exposure modes and saves a sample image of 
-that mode in different resolutions defined in the 'resolutions' tuple
-'''
+def iso_test(cam):
+    if not os.path.exists(__file__[:__file__.rfind('/')+1]+'iso_shots/'):
+        os.makedirs(__file__[:__file__.rfind('/')+1]+'iso_shots/')
+    cam.start_preview()
+    cam.annotate_text = 'Beginning iso Tests!'
+    sleep(0.5)
+    for iso in range(100,800,50):
+        cam.iso = int(iso)
+        cam.annotate_text = 'ISO - '+str(iso)
+        sleep(1.5)
+        cam.capture(__file__[:__file__.rfind('/')+1]+'iso_shots/iso'+str(iso)+'.jpg')
+    cam.stop_preview()
+    
+def exposure_test(cam):
+    resolutions = [cam.MAX_RESOLUTION, (1280,900), (800,600)]
+    cam.start_preview()
+    cam.annotate_text = 'Beginning Exposure Tests!'
+    sleep(0.5)
+    for res in resolutions:
+        cam.resolution = res
+        if not os.path.exists(__file__[:__file__.rfind('/')+1]+str(res[0])+'x'+str(res[1])+'/'):
+            os.makedirs(__file__[:__file__.rfind('/')+1]+str(res[0])+'x'+str(res[1])+'/')
+        for mode in cam.EXPOSURE_MODES:
+            cam.exposure_mode = str(mode)
+            try:
+                cam.annotate_text = str(mode)
+                sleep(2)
+                cam.capture(__file__[:__file__.rfind('/')+1]+str(res[0])+'x'+str(res[1])+'/'+str(mode)+'.jpg')
+            except:
+                print('error occurred during '+str(mode)+res)
+    cam.annotate_text = 'Exposure Modes Complete!'
+    sleep(0.5)
+    cam.stop_preview()
 
-cam = PiCamera()
-#change rotation based off hardware camera positioning.
-#rotation 0 if hardware ribbon is at the bottom of the camera module
-cam.rotation = 180
-cam.framerate = 25
-cam.awb_mode = 'auto'
-
-resolutions = (cam.MAX_RESOLUTION,(1280,900),(800,600))
-
-#get camera rolling...
-cam.start_preview()
-for res in resolutions:
-	cam.resolution = res
-	(os.makedirs(__file__[:__file__.rfind('/')+1]+str(res[0])+'x'+str(res[1])+'/'),None) \
-	[os.path.exists(__file__[:__file__.rfind('/')+1]+str(res[0])+'x'+str(res[1])+'/')]
-	for mode in cam.EXPOSURE_MODES:
-		cam.exposure_mode = str(mode)
-		#begin attempt to capture image with an annotation describing mode
-		try:
-			cam.annotate_text = str(mode)
-			#give camera CMOS sensor time to adjust image to proper exposure
-			sleep(2)
-			cam.capture(__file__[:__file__.rfind('/')+1]+str(res[0])+'x'+str(res[1])+'/'+str(mode)+'.jpg')
-		except:
-			print('error occured during capture of '+str(mode)+' '+res)
-cam.annotate_text = 'Test has concluded...'
-sleep(2)
-cam.annotate_text = 'Good Bye!'
-sleep(1)
-cam.stop_preview()
-cam.close()	
+def imgfx_test(cam):
+    resolutions = [cam.MAX_RESOLUTION, (1280,900), (800,600)]
+    cam.start_preview()
+    cam.annotate_text = 'Starting Image Effects!'
+    sleep(0.5)
+    if not os.path.exists(__file__[:__file__.rfind('/')+1]+'image_fx/'):
+        os.makedirs(__file__[:__file__.rfind('/')+1]+'image_fx/')
+        
+    for ifx in cam.IMAGE_EFFECTS:
+        cam.resolution = resolutions[1]
+        cam.image_effect = ifx
+        cam.exposure_mode = 'auto'
+        cam.annotate_text = str(ifx)
+        sleep(0.75)
+        cam.capture(__file__[:__file__.rfind('/')+1]+'image_fx/'+str(ifx)+'.jpg')
+    cam.annotate_text = 'Test Complete!'
+    sleep(0.5)
+    cam.stop_preview()
